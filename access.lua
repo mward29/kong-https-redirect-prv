cb8f310c761af9490eb281ffb64527394b935744
jdarling <jeremy.darling@gmail.com>
2017-04-11 15:07:32 -0500

    Add in logging options

--- access.lua
+++ access.lua
@@ -9,13 +9,21 @@ local _M = {}
 
 function _M.execute(conf)
   local scheme = (ngx.var.scheme or ""):lower()
+  local logging_enabled = conf.logging_enabled
+  local debug_logging_enabled = conf.debug_logging_enabled
+
+  if debug_logging_enabled then
+    log("Request for " .. scheme .. "://"..ngx.var.host .. ngx.var.request_uri)
+  end
   if scheme == 'http' then
     local redirect_url = "https://" .. ngx.var.host
     if conf.port ~= "443" then
       redirect_url = redirect_url .. ":" .. conf.port
     end
     redirect_url = redirect_url .. ngx.var.request_uri
-    log("Request for http://"..ngx.var.host .. ngx.var.request_uri .. " redirected to "..redirect_url)
+    if logging_enabled then
+      log("Request for http://"..ngx.var.host .. ngx.var.request_uri .. " redirected to "..redirect_url)
+    end
     ngx.redirect(redirect_url)
   end
 end
--- schema.lua
+++ schema.lua
@@ -2,5 +2,7 @@ return {
   -- no_consumer = true,
   fields = {
     port = { type = "string", default = 443 },
+    logging_enabled = { type = "boolean", default = true },
+    debug_logging_enabled = { type = "boolean", default = false },
   }
 }
