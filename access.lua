0f0e783c6f60c3ec0f99fb89b5d69aadf0c41f62
jdarling <jeremy.darling@gmail.com>
2017-04-11 14:43:06 -0500

    Add in better logging

--- access.lua
+++ access.lua
@@ -1,6 +1,10 @@
 local url = require "socket.url"
 local pl_stringx = require "pl.stringx"
 
+local function log(...)
+  ngx.log(ngx.ERR, ...)
+end
+
 local _M = {}
 
 function _M.execute(conf)
@@ -11,8 +15,8 @@ function _M.execute(conf)
       redirect_url = redirect_url .. ":" .. conf.port
     end
     redirect_url = redirect_url .. ngx.var.request_uri
+    log("Request for http://"..ngx.var.host .. ngx.var.request_uri .. " redirected to "..redirect_url)
     ngx.redirect(redirect_url)
-    return
   end
 end
 
